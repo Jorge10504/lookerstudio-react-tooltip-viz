0805066e9cc13bc5aa8944b4c4e9b9c72020593d
Jorge M. Rodriguez <georgeunal2016@gmail.com>
2026-02-24 17:28:56 -0500

    Fixing base

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "./", // IMPORTANT for GitHub Pages + Looker Studio iframe
   plugins: [react()],
   build: {
     rollupOptions: {
