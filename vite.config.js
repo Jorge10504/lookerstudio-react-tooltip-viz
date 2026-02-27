eda9e684658f343b723b06df92334956ce602e08
Jorge M. Rodriguez <georgeunal2016@gmail.com>
2026-02-27 06:10:04 -0500

    Fixing error Looker Studio

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "./", // IMPORTANT for GitHub Pages + Looker Studio iframe
-  plugins: [react()],
+  base: "./",
   build: {
     rollupOptions: {
       output: {
         entryFileNames: "index.js",
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name && assetInfo.name.endsWith(".css")) return "index.css";
+          if (assetInfo.name && assetInfo.name.endsWith(".css"))
+            return "index.css";
           return "assets/[name][extname]";
         }
       }
